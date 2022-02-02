20e6e3b1c733a2c8689aa62fde5a525812a66154
jehingson <jehingson25@gmail.com>
2022-02-01 23:26:15 -0500

    [fix]

--- src/server.js
+++ src/server.js
@@ -53,13 +53,6 @@ app.use(express.static('public'))
 app.use(cors());
 app.use(graphqlUploadExpress());
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client','build', 'index.html' ))
-  })
-}
-
 
 const port = process.env.PORT || 3000;
 httpServer.listen({ port }, () => {
